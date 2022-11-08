01481595724d952c01740237a30165992215234d
Stephen Koch <stephen@koch.dev>
2022-11-08 10:55:11 -0800

    react build
    complete overhaul of build system for react, thanks shoelace

---
+++ scripts/build.js
@@ -0,0 +1,88 @@
+import fs from 'fs';
+import chalk from 'chalk';
+import esbuild from 'esbuild';
+import { globby } from 'globby';
+import { deleteSync } from 'del';
+import { execSync } from 'child_process';
+import commandLineArgs from 'command-line-args';
+
+const { bundle, dir, types } = commandLineArgs([
+  { name: 'bundle', type: Boolean },
+  { name: 'dir', type: String, defaultValue: 'dist' },
+  { name: 'types', type: Boolean },
+]);
+
+const outdir = dir;
+
+deleteSync(outdir);
+fs.mkdirSync(outdir, { recursive: true });
+
+(async () => {
+  try {
+    execSync(`node scripts/make-metadata.js --outdir "${outdir}"`, {
+      stdio: 'inherit',
+    });
+    execSync(`node scripts/make-react.js --outdir "${outdir}"`, {
+      stdio: 'inherit',
+    });
+
+    if (types) {
+      console.log(chalk.yellow('Running the TypeScript compiler...'));
+      execSync(`tsc --outdir "${outdir}"`, {
+        stdio: 'inherit',
+      });
+    }
+  } catch (err) {
+    console.error(chalk.red(err));
+    process.exit(1);
+  }
+
+  const alwaysExternal = ['@lit-labs/react', 'react'];
+  const buildResult = await esbuild
+    .build({
+      format: 'esm',
+      target: 'es2017',
+      entryPoints: [
+        // The whole shebang
+        './src/index.ts',
+        // Components
+        ...(await globby('./src/artistic/**/*.ts')),
+        // React wrappers
+        ...(await globby('./src/react/**/*.ts')),
+      ],
+      outdir,
+      chunkNames: 'chunks/[name].[hash]',
+      // incremental: serve,
+      incremental: false,
+      define: {
+        // Floating UI requires this to be set
+        'process.env.NODE_ENV': '"production"',
+      },
+      // minify: true,
+      bundle: true,
+      //
+      // We don't bundle certain dependencies in the unbundled build. This ensures we ship bare module specifiers,
+      // allowing end users to better optimize when using a bundler. (Only packages that ship ESM can be external.)
+      //
+      // We never bundle React or @lit-labs/react though!
+      //
+      // external: alwaysExternal,
+      external: bundle ? alwaysExternal : [...alwaysExternal, 'lit'],
+      splitting: true,
+      plugins: [],
+    })
+    .catch(err => {
+      console.error(chalk.red(err));
+      process.exit(1);
+    });
+
+  // Copy the build output to an additional directory
+  // if (copydir) {
+  //   deleteSync(copydir);
+  //   copy(outdir, copydir);
+  // }
+
+  console.log(chalk.green(`The build has been generated at ${outdir} 📦\n`));
+
+  process.on('SIGTERM', () => buildResult.rebuild.dispose());
+})();
---
+++ scripts/make-react.js
@@ -0,0 +1,74 @@
+import fs from 'fs';
+import path from 'path';
+import chalk from 'chalk';
+import prettier from 'prettier';
+import { deleteSync } from 'del';
+import commandLineArgs from 'command-line-args';
+
+import { getAllComponents } from './shared.js';
+import prettierConfig from '../prettier.config.cjs';
+
+const { outdir } = commandLineArgs({ name: 'outdir', type: String });
+
+const reactDir = path.join('./src/react');
+
+// Clear build directory
+deleteSync(reactDir);
+fs.mkdirSync(reactDir, { recursive: true });
+
+// Fetch component metadata
+const metadata = JSON.parse(
+  fs.readFileSync(path.join(outdir, 'custom-elements.json'), 'utf8'),
+);
+
+// Wrap components
+console.log(chalk.yellow('Wrapping components for React...'));
+
+const components = getAllComponents(metadata);
+const index = [];
+
+components.map(component => {
+  const tagWithoutPrefix = component.tagName.replace(/^artistic-/, '');
+  // const componentDir = path.join(reactDir, tagWithoutPrefix);
+  // const componentFile = path.join(componentDir, 'index.ts');
+  const componentFile = path.join(reactDir, `${tagWithoutPrefix}.ts`);
+  const importPath = component.modulePath
+    .replace(/^src\//, '')
+    .replace(/\.ts$/, '');
+  const events = (component.events || [])
+    .map(event => `${event.reactName}: '${event.name}'`)
+    .join(',\n');
+
+  // fs.mkdirSync(componentDir, { recursive: true });
+
+  const source = prettier.format(
+    `
+      import * as React from 'react';
+      import { createComponent } from '@lit-labs/react';
+      import { ${component.name} } from '../${importPath}';
+
+      export default createComponent({
+        tagName: '${component.tagName}',
+        elementClass: ${component.name},
+        react: React,
+        events: {
+          ${events}
+        },
+      });
+    `,
+    Object.assign(prettierConfig, {
+      parser: 'babel-ts',
+    }),
+  );
+
+  index.push(
+    `export { default as ${component.name} } from './${tagWithoutPrefix}';`,
+  );
+
+  fs.writeFileSync(componentFile, source, 'utf8');
+});
+
+// Generate the index file
+fs.writeFileSync(path.join(reactDir, 'index.ts'), index.join('\n'), 'utf8');
+
+console.log(chalk.cyan(`\nComponents have been wrapped for React! 📦\n`));
